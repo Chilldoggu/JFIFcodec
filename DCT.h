ea88d06f5037aba0de980f17c7d5eea2a33d6a4e
Chilldoggu <264120@student.pwr.edu.pl>
2024-01-22 20:11:29 +0100

    DCT_improved

--- DCT.h
+++ DCT.h
@@ -5,11 +5,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static double M[8][8];
+// #define PTRTOARR(ARR, N, M, PTR) do{        \
+//         for (size_t i = 0; i < N; i++) {    \
+//             for (size_t j = 0; j < M; j++)  \
+//                 ARR[i][j] = PTR[i][j];      \
+//             free(PTR[i]);                   \
+//         }                                   \
+//         free(PTR);                          \
+//         } while (0);
+
+#define FREE_MATRIX(MATRIX, N) do { \
+        for (int i = 0; i < N; ++i) \
+            free(MATRIX[i]);        \
+        free(MATRIX);               \
+        } while (0);                \
+
+static double **M;
 
 int init_DCT()
 {
+    M = (double**)malloc(sizeof(double*)*8);
     for (size_t i = 0; i < 8; i++) {
+        M[i] = (double*)malloc(sizeof(double)*8);
         M[0][i] = 1/sqrt(8);
     }
     for (size_t i = 0; i < 8; i++) {
@@ -21,7 +38,7 @@ int init_DCT()
     return 0;
 }
 
-double **matrix_transpose(int m, int n, double A[][m])
+double **matrix_transpose(int m, int n, double **A)
 {
     double **B_out = (double**)malloc(sizeof(double*)*n);
 
@@ -44,7 +61,7 @@ double vector_mult(int n, double *v1, double *v2)
     return r;
 }
 
-double **matrix_mult(int ma, int na, double A[][ma], int mb, int nb, double B[][mb])
+double **matrix_mult(int ma, int na, double **A, int mb, int nb, double **B)
 {
     /* Check if multiplication is possible */
     if (na != mb || ma != nb) 
@@ -100,6 +117,133 @@ double *IDCT_1dim(double *T_in, int N)
     }
     
     return V_out;
+}
+
+double **DCT_quant(double **DCT_table, unsigned char *q_table)
+{
+    double **q_dct = (double**)malloc(sizeof(double*)*8);
+    for (size_t i = 0; i < 8; i++) {
+        q_dct[i] = (double*)malloc(sizeof(double)*8);
+        for (size_t j = 0; j < 8; j++)
+            DCT_table[i][j] = (int)round(DCT_table[i][j]/q_table[i*8+j]);
+    }
+
+    return 0;
+}
+
+int **IDCT_quant(double **IDCT_table, unsigned char *q_table)
+{
+    for (size_t i = 0; i < 8; i++) {
+        for (size_t j = 0; j < 8; j++)
+            IDCT_table[i][j] *= q_table[i*8+j];
+    }
+    
+    return 0;
+}
+
+/* Size is alway 8x8 */
+double **DCT_2dim(double **V, unsigned char *Q)
+{
+    double **M_T = matrix_transpose(8, 8, M);
+    double **MV  = matrix_mult(8, 8, M, 8, 8, V);
+    double **T   = matrix_mult(8, 8, MV, 8, 8, M_T);
+    double **T_rounded = (double**)malloc(sizeof(double*)*8);
+
+    for (size_t i = 0; i < 8; i++) {
+        T_rounded[i] = (double*)malloc(sizeof(double)*8);
+        for (size_t j = 0; j < 8; j++) {
+            T_rounded[i][j] = (double)round(T[i][j]);
+        }
+    }
+
+    /* Perform quantization */
+    DCT_quant(T_rounded, Q);
+
+    /* Clear */
+    FREE_MATRIX(T, 8);
+    FREE_MATRIX(MV, 8);
+    FREE_MATRIX(M_T, 8);
+
+    return T_rounded;
+}
+
+/* Size is alway 8x8 */
+double **IDCT_2dim(double **T, unsigned char *Q)
+{
+    /* Perform dequantization */
+    IDCT_quant(T, Q);
+
+    double **M_T   = matrix_transpose(8, 8, M);
+    double **M_TT  = matrix_mult(8, 8, M_T, 8, 8, T);
+    
+    double **V   = matrix_mult(8, 8, M_TT, 8, 8, M);
+    double **V_rounded = (double**)malloc(sizeof(double*)*8);
+
+    for (size_t i = 0; i < 8; i++) {
+        V_rounded[i] = (double*)malloc(sizeof(double)*8);
+        for (size_t j = 0; j < 8; j++) {
+            V_rounded[i][j] = round(V[i][j]);
+        }
+    }
+
+    /* Clear */
+    FREE_MATRIX(M_T, 8);
+    FREE_MATRIX(M_TT, 8);
+    FREE_MATRIX(V, 8);
+
+    return V_rounded;
+}
+
+unsigned char *zigzag_decode_q(unsigned char *bytes)
+{
+    int iter = 0, i = 0, j = 0, loop_up = 0, loop_down = 0;
+    unsigned char Q_tmp[8][8] = {};
+    /* Decode 64 byte table of quantization values into array */
+    unsigned char *Q = (unsigned char*)malloc(sizeof(unsigned char)*64);
+
+    int n = 3, m = 2;
+    while (iter != n*m) {
+        Q_tmp[i][j] = bytes[iter];
+        // Q_tmp[i][j] = iter;
+        if (!loop_up && !loop_down) {
+            if (i == 0 && j < m || i == n-1) {
+                j++;
+            } else if (j == 0 && i < n || j == m-1) {
+                i++;
+            }
+
+            if (i == 0 || j == m-1)
+                loop_down = 1;
+            else if (j == 0 || i == n-1)
+                loop_up = 1;            
+            iter++;
+            continue;
+        }
+
+        if (loop_down) {            
+            i++;
+            j--;
+        } else if (loop_up) {
+            i--;
+            j++;
+        }
+
+        if (j == 0 || j == m-1 || i == 0 || i == n-1) {
+            loop_down = 0;
+            loop_up = 0;
+        }
+
+        iter++;
+    }
+
+    for (size_t i = 0; i < 8; i++)
+        for (size_t j = 0; j < 8; j++) {
+            Q[i*8+j] = Q_tmp[i][j];
+            // printf("%3d%c", Q_tmp[i][j], (j == 7) ? '\n' : ' ');
+        }
+    // printf("\n");
+
+    return Q;
 }
 
 #endif
--- main.c
+++ main.c
@@ -1,64 +1,57 @@
-// #include "jfif_dump.h"
+#include "jfif_dump.h"
 #include "DCT.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 #define DEBUG(txt) printf(txt"\n")
 
-int main()
+int main(int argc, char*argv[])
 {
+    char *filename = argv[1];
+    // handle_loop(filename);
     init_DCT();
+    zigzag_decode_q("");
 
-    int N = 8;
-    double V_in[] = {128, 88, 40, 0, 0, 40, 88, 128};
-
-    double A[2][3] = {
-        {1, 0, 2}, 
-        {-1, 3, 1}
+    // Sample Y component
+    int V_reference[8][8] = {
+        {58,  45,  29,  27, 24, 19, 17, 20},
+        {62,  52,  42,  41, 38, 30, 22, 18},
+        {48,  47,  49,  44, 40, 36, 31, 25},
+        {59,  78,  49,  32, 28, 31, 31, 31},
+        {98,  138, 116, 78, 39, 24, 25, 27},
+        {115, 160, 143, 97, 48, 27, 24, 21},
+        {99,  137, 127, 84, 42, 25, 24, 20},
+        {74,  95,  82,  67, 40, 25, 25, 19}
     };
 
-    double B[3][2] = {
-        {3, 1},
-        {2, 1},
-        {1, 0}
-    };
+    unsigned char Q_Y_ref[64] = {16, 11, 10, 16, 24,  40,  51,  61, 12, 12, 14, 19, 26,  58,  60,  55 ,14, 13, 16, 24, 40,  57,  69,  56 ,14, 17, 22, 29, 51,  87,  80,  62 ,18, 22, 37, 56, 68,  109, 103, 77 ,24, 35, 55, 64, 81,  104, 113, 92, 49, 64, 78, 87, 103, 121, 120, 101 ,72, 92, 95, 98, 112, 100, 103, 99};
 
-    double **C_out = matrix_mult(3, 2, A, 2, 3, B);
-    double C[2][2];
-    for (size_t i = 0; i < 2; i++)
-        for (size_t j = 0; j < 2; j++)
-            C[i][j] = C_out[i][j];
+    double **V_in = (double**)malloc((sizeof(double*)*8));
+    for (size_t i = 0; i < 8; i++) {
+        V_in[i] = (double*)malloc((sizeof(double)*8));
+        for (size_t j = 0; j < 8; j++)
+            V_in[i][j] = V_reference[i][j];
+    }
     
-    double **C_T   = matrix_transpose(2, 2, C);
+    /* Get DCT coefficients after quantization */
+    double **T = DCT_2dim((double**)V_in, Q_Y_ref);
 
-    for (size_t i = 0; i < 2; i++) {
-        for (size_t j = 0; j < 2; j++) {
-            printf("%f ", C_out[i][j]);
-        }
-        printf("\n");
-    }
-
-    for (size_t i = 0; i < 2; i++) {
-        for (size_t j = 0; j < 2; j++) {
-            printf("%f ", C_T[i][j]);
-        }
-        printf("\n");
-    }
-
-    double *T_out = DCT_1dim(V_in, N);
-
-    for (size_t i = 0; i < N; i++)
-        printf("%f ", T_out[i]);
+    for (size_t i = 0; i < 8; i++)
+        for (size_t j = 0; j < 8; j++)
+            printf("%4.0f%c", T[i][j], (j == 7) ? '\n' : ' ');
     printf("\n");
 
-    double *V_out = IDCT_1dim(T_out, N);
+    /* Get Y component from DCT coefficients after dequantization */
+    double **V = IDCT_2dim(T, Q_Y_ref);
 
-    for (size_t i = 0; i < N; i++) {
-        printf("%f ", V_out[i]);
-    } printf("\n");
-
-    free(T_out);
-    free(V_out);
+    for (size_t i = 0; i < 8; i++)
+        for (size_t j = 0; j < 8; j++)
+            printf("%4.0f%c", V[i][j], (j == 7) ? '\n' : ' ');
+    
+    /* Clear */
+    FREE_MATRIX(T, 8);
+    FREE_MATRIX(V, 8);
+    FREE_MATRIX(V_in, 8);
 
     return 0;
 }
